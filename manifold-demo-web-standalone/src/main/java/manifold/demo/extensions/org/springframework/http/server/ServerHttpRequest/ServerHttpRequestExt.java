3a3d1511153eb66538d1f76bd34ba00fe2a909f6
xkyii <xkyii@foxmail.com>
2022-01-03 20:01:13 +0800

    Add: manifold-demo-web-standalone, worked fine

---
+++ manifold-demo-web-standalone/src/main/java/manifold/demo/extensions/org/springframework/http/server/ServerHttpRequest/ServerHttpRequestExt.java
@@ -0,0 +1,17 @@
+package manifold.demo.extensions.org.springframework.http.server.ServerHttpRequest;
+
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+import org.springframework.http.server.ServerHttpRequest;
+
+@Extension
+public class ServerHttpRequestExt {
+  public static void helloWorld(@This ServerHttpRequest thiz, String hello) {
+    if (hello.isNullOrEmpty()) {
+      System.out.println("isNullOrEmpty");
+    }
+    else {
+      System.out.println("hello world!");
+    }
+  }
+}
