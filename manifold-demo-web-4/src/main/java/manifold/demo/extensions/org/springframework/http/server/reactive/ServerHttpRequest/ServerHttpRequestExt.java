5960f402c982fdaac625fe7b0664f9ecd35300bd
xkyii <xkyii@foxmail.com>
2022-01-03 17:50:13 +0800

    Up: rename package

---
+++ manifold-demo-web-4/src/main/java/manifold/demo/extensions/org/springframework/http/server/reactive/ServerHttpRequest/ServerHttpRequestExt.java
@@ -0,0 +1,19 @@
+package manifold.demo.extensions.org.springframework.http.server.reactive.ServerHttpRequest;
+
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+// cannot find symbols
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+
+@Extension
+public class ServerHttpRequestExt {
+  // cannot find symbols
+  // uncomment this method, compile failed.
+  public static void helloWorld(@This ServerHttpRequest thiz, String hello) {
+    if (hello.isNullOrEmpty()) {
+      System.out.println("bye!");
+    }
+    System.out.println("hello " + hello + "!");
+  }
+}
---
+++ manifold-demo-web/src/main/java/manifold/demo/extensions/org/springframework/http/server/reactive/ServerHttpRequest/ServerHttpRequestExt.java
@@ -0,0 +1,12 @@
+package manifold.demo.extensions.org.springframework.http.server.reactive.ServerHttpRequest;
+
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+@Extension
+public class ServerHttpRequestExt {
+  public static void helloWorld(@This ServerHttpRequest thiz) {
+    System.out.println("hello world!");
+  }
+}
